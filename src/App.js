ce90397d4a85196c3248c51a6d7f496e13d66c12
Randark <koldaev.alexey@gmail.com>
2022-08-26 23:55:36 +0300

    настроил инпут

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
-import React from 'react';
-import { Block } from './Block';
-import './index.scss';
+import React from "react";
+import axios from "axios";
+
+import { Block } from "./Block";
+import "./index.scss";
 
 function App() {
-  return (
-    <div className="App">
-      <Block value={0} currency="RUB" onChangeCurrency={(cur) => console.log(cur)} />
-      <Block value={0} currency="USD" />
-    </div>
-  );
+    const [valutes, setValutes] = React.useState({});
+    const [toLeft, setToleft] = React.useState(1);
+    const [toRight, setToRight] = React.useState(1);
+
+    React.useEffect(() => {
+        axios.get("https://cdn.cur.su/api/latest.json/").then((res) => {
+            setValutes(res.data.rates);
+        });
+    }, []);
+
+    return (
+        <div className="App">
+            <Block
+                value={toLeft}
+                currency="RUB"
+                onChangeCurrency={(cur) => console.log(cur)}
+                onChangeValue={setToleft}
+            />
+            <Block
+                value={toRight}
+                currency="USD"
+                onChangeCurrency={(cur) => console.log(cur)}
+                onChangeValue={setToRight}
+            />
+        </div>
+    );
 }
 
 export default App;
