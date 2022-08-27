17b30c090fe084f382fc7b7454f449943f1537b9
Randark <koldaev.alexey@gmail.com>
2022-08-27 23:59:14 +0300

    .

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ function App() {
     const [valutes, setValutes] = React.useState({});
     const [toLeft, setToleft] = React.useState(1);
     const [toRight, setToRight] = React.useState(1);
+    const [leftCur, setLeftCur] = React.useState("RUB");
+    const [rigthCur, setRightCur] = React.useState("USD");
 
     React.useEffect(() => {
         axios.get("https://cdn.cur.su/api/latest.json/").then((res) => {
@@ -15,19 +17,30 @@ function App() {
         });
     }, []);
 
+    const changeFromValute = (value) => {
+        setToleft(value);
+    };
+
+    const changeToValute = (value) => {
+        const price = value / valutes[leftCur];
+        const result = price * valutes[rigthCur];
+        setToleft(result);
+        setRightCur(value);
+    };
+
     return (
         <div className="App">
             <Block
                 value={toLeft}
-                currency="RUB"
-                onChangeCurrency={(cur) => console.log(cur)}
+                currency={leftCur}
+                onChangeCurrency={setLeftCur}
                 onChangeValue={setToleft}
             />
             <Block
                 value={toRight}
-                currency="USD"
-                onChangeCurrency={(cur) => console.log(cur)}
-                onChangeValue={setToRight}
+                currency={rigthCur}
+                onChangeCurrency={setRightCur}
+                onChangeValue={changeFromValute}
             />
         </div>
     );
