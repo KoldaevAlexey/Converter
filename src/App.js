96d6e61da5eac25dcad90837cd03d45d16ff50ad
Randark <koldaev.alexey@gmail.com>
2022-08-28 16:22:33 +0300

    Основная механика конвертера готова

--- src/App.js
+++ src/App.js
@@ -5,42 +5,61 @@ import { Block } from "./Block";
 import "./index.scss";
 
 function App() {
-    const [valutes, setValutes] = React.useState({});
-    const [toLeft, setToleft] = React.useState(1);
-    const [toRight, setToRight] = React.useState(1);
-    const [leftCur, setLeftCur] = React.useState("RUB");
-    const [rigthCur, setRightCur] = React.useState("USD");
+    const [leftInput, setLeftInput] = React.useState(0);
+    const [rightInput, setRightInput] = React.useState(1);
+    const [leftValuteTitle, setLeftValuteTitle] = React.useState("RUB");
+    const [rightValuteTitle, setRightValuteTitle] = React.useState("USD");
+
+    const valutes = React.useRef({});
 
     React.useEffect(() => {
         axios.get("https://cdn.cur.su/api/latest.json/").then((res) => {
-            setValutes(res.data.rates);
+            valutes.current = res.data.rates;
+            convertRightToLeft(1);
         });
     }, []);
 
-    const changeFromValute = (value) => {
-        setToleft(value);
+    const convertRightToLeft = (value) => {
+        setRightInput(value);
+        setLeftInput(
+            (
+                (value / valutes.current[rightValuteTitle]) *
+                valutes.current[leftValuteTitle]
+            ).toFixed(2)
+        );
     };
 
-    const changeToValute = (value) => {
-        const price = value / valutes[leftCur];
-        const result = price * valutes[rigthCur];
-        setToleft(result);
-        setRightCur(value);
+    const convertLeftToRight = (value) => {
+        setLeftInput(value);
+        setRightInput(
+            (
+                (value / valutes.current[leftValuteTitle]) *
+                valutes.current[rightValuteTitle]
+            ).toFixed(2)
+        );
     };
+
+    React.useEffect(() => {
+        convertLeftToRight(leftInput);
+    }, [leftValuteTitle]);
+
+    React.useEffect(() => {
+        convertRightToLeft(rightInput);
+    }, [rightValuteTitle]);
 
     return (
         <div className="App">
             <Block
-                value={toLeft}
-                currency={leftCur}
-                onChangeCurrency={setLeftCur}
-                onChangeValue={setToleft}
+                value={leftInput}
+                currency={leftValuteTitle}
+                onChangeCurrency={setLeftValuteTitle}
+                onChangeValue={convertLeftToRight}
             />
             <Block
-                value={toRight}
-                currency={rigthCur}
-                onChangeCurrency={setRightCur}
-                onChangeValue={changeFromValute}
+                value={rightInput}
+                currency={rightValuteTitle}
+                onChangeCurrency={setRightValuteTitle}
+                onChangeValue={convertRightToLeft}
             />
         </div>
     );
